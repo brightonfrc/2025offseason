40cbf0549c7b41ad0b3db67c8b4c9bb464490d55
Oliver Geer <hi@webcoder49.dev>
2025-01-22 16:40:31 +0000

    Add stub AprilTagPoseEstimator subsystem

---
+++ src/main/java/frc/robot/subsystems/AprilTagPoseEstimator.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AprilTagPoseEstimator extends SubsystemBase {
+  /** Creates a new AprilTagPoseEstimator. */
+  public AprilTagPoseEstimator() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public Optional<Transform3d> getRobotToTag(int tagID) {
+    // Stub
+    return Optional.empty();
+  }
+}
