c360bcb9a4764bea62cdfe8bea59904882736a9a
Ethan Harjabrata <ethanjhar@gmail.com>
2025-02-09 17:22:12 +0800

    Draft version for testing
    Remember to tune all the constants in SmartDashBoard

---
+++ src/main/java/frc/robot/commands/Lift.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DatisLift;
+import frc.robot.Constants.LiftConstants;
+import frc.robot.Constants.LiftConstants.Height;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Lift extends Command {
+  private DatisLift lift;
+  private double angleRequired;
+  private PIDController liftController;
+  /** Creates a new Lift. */
+  public Lift(DatisLift lift, Height heightDesired) {
+    this.lift=lift;
+    double height=0;
+    switch(heightDesired){
+      case Ground:
+        height=LiftConstants.desiredHeight[0];
+        break;
+      case L1:
+        height=LiftConstants.desiredHeight[1];
+        break;
+      case L2:
+        height=LiftConstants.desiredHeight[2];
+        break;
+      case L3:
+        height=LiftConstants.desiredHeight[3];
+        break;
+      case L4:
+        height=LiftConstants.desiredHeight[4];
+        break;
+    }
+    angleRequired=Math.asin(height/2/LiftConstants.armLength);
+    SmartDashboard.putNumber("Angle required", angleRequired);
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(lift);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    liftController= new PIDController(LiftConstants.kPLift, LiftConstants.kILift, LiftConstants.kDLift);
+    liftController.setTolerance(LiftConstants.angleTolerance);
+    liftController.setSetpoint(angleRequired);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double currentAngle=lift.getLiftAngle();
+    SmartDashboard.putNumber("Current Lift angle", currentAngle);
+    double desiredPower=liftController.calculate(currentAngle)+LiftConstants.kWeightMomentOffsetFactor*Math.cos(currentAngle);
+    SmartDashboard.putNumber("Power", desiredPower);
+    lift.setPower(desiredPower);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    lift.setPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return liftController.atSetpoint();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DatisLift.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LiftConstants;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.AbsoluteEncoder;
+public class DatisLift extends SubsystemBase {
+  private SparkMax liftNeo;
+  private AbsoluteEncoder encoder;
+  /** Creates a new DatisLift. */
+  public DatisLift(SparkMax liftNeo) {
+    this.liftNeo=liftNeo;
+
+    encoder=liftNeo.getAbsoluteEncoder();
+    
+    
+  }
+  /**Method to get the current lift angle (0 being at lowest level) */
+  public double getLiftAngle(){
+    return encoder.getPosition()*LiftConstants.rotationConversionFactor;
+  }
+  /**Method used to set lift power ranging from -1 to 1 */
+  public void setPower(double power){
+    liftNeo.set(power);
+  }
+  
+}
