e87cb2f6ef378fba3fac10e6b1fd7eb4d2e91999
Ethan Harjabrata <ethanjhar@gmail.com>
2025-02-16 19:44:21 +0800

    temp
    Added smardashboard stuff for testing

--- src/main/java/frc/robot/commands/RunLift.java
+++ src/main/java/frc/robot/commands/RunLift.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 import frc.robot.subsystems.DatisLift;
 import frc.robot.Constants.LiftConstants;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
@@ -28,6 +28,7 @@ public class RunLift extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    SmartDashboard.putNumber("Lift Angle", lift.getLiftAngle());
     if (up){
       lift.setPower(LiftConstants.maxPower);
     }
